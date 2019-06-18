c2504c09cd11c8361714c6c840fb29f9f966f864
falconiq <48307943+falconiq@users.noreply.github.com>
2019-06-18 03:40:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("#PLAY",{type: 'WATCHING'})
+   client.user.setActivity("FALCON-IQ",{type: 'BY'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
